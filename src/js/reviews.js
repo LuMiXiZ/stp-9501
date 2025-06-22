2d93a8ec9ba09c284f7ef4d3a624dccbe327fe85
Misha Busko <Misha.1996@ukr.net>
2025-06-22 19:18:36 +0300

    Refactor swiper components and styles for improved layout and functionality

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,32 +1,48 @@
 import Swiper from 'swiper';
-import { Autoplay, Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 document.addEventListener('DOMContentLoaded', function () {
   const swiper = new Swiper('.reviews-swiper', {
-    modules: [Navigation, Pagination, Autoplay],
 
-    // loop: true,
-
-    initialSlide: 0,
-
+    slidesPerView: 3,
+    direction: 'vertical',
+    spaceBetween: 32,
+    modules: [Navigation, Pagination],
     pagination: {
       el: '.reviews-swiper-pagination',
       clickable: true,
-      type: 'bullets',
     },
-
     navigation: {
       nextEl: '.reviews-swiper-button-next',
       prevEl: '.reviews-swiper-button-prev',
     },
-
     breakpoints: {
-      0: { slidesPerView: 1, spaceBetween: 32, centeredSlides: false },
-      1440: { slidesPerView: 1, spaceBetween: 10, centeredSlides: false },
+      1200: {
+        direction: 'horizontal',
+        spaceBetween: 20,
+        slidesPerView: 3,
+      },
     },
+
+    // initialSlide: 0,
+
+    // pagination: {
+    //   el: '.reviews-swiper-pagination',
+    //   clickable: true,
+    //   type: 'bullets',
+    // },
+
+    // navigation: {
+    //   nextEl: '.reviews-swiper-button-next',
+    //   prevEl: '.reviews-swiper-button-prev',
+    // },
+
+    // breakpoints: {
+    //   0: { slidesPerView: 1, spaceBetween: 32, centeredSlides: false },
+    //   1440: { slidesPerView: 1, spaceBetween: 10, centeredSlides: false },
+    // },
   });
 });
